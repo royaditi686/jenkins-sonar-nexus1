59ebcad00d3b2e4a3d9591250efaf164b48f9010
royaditi686 <royaditi686@gmail.com>
2026-05-05 17:55:45 +0530

    Update nexus-server.tf

--- nexus-server.tf
+++ nexus-server.tf
@@ -15,7 +15,7 @@
 
 resource "aws_instance" "nexus-server" {   # we are creating a new instance for jenkins-server
     ami = data.aws_ami.latest.id    # we are using the latest ami that we fetched earlier
-    instance_type = "c7i-flex.large"     # This is the type of the instance we are creating
+    instance_type = "c7i.flex-large"     # This is the type of the instance we are creating
     subnet_id = aws_subnet.our-public-subnet.id   # this is the id of the subnet we are using to launch the instance
     user_data = file("./nexus-server.sh")  # this is the script that will be executed during the creation of the instance
     key_name = "terraform" # this is the key name that we have created in console
@@ -28,58 +28,4 @@ resource "aws_instance" "nexus-server" {   # we are creating a new instance for 
     tags = {
         Name = "nexus-server"  # this will provide name to instance 
     }
-}# data "aws_ami" "latest" {     # aws_ami helps to get AMI ID of the os
-#     most_recent = true  # this is the filter for most recent Ami
-   
-#     filter {   # this is the filter for virtualization type
-#       name = "virtualization-type"
-#       values = ["hvm"]
-#     }
-#     filter {  # this is the filter for AMI name of the OS which can be found in AMI section in EC2
-#         name = "name"
-#         values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-#     }
-#      owners = ["099720109477"]  # this is the owner of the OS, which can be found in AMI section in EC2
-# }
-
-
-resource "aws_instance" "nexus-server" {   # we are creating a new instance for jenkins-server
-    ami = data.aws_ami.latest.id    # we are using the latest ami that we fetched earlier
-    instance_type = "t3.medium"     # This is the type of the instance we are creating
-    subnet_id = aws_subnet.our-public-subnet.id   # this is the id of the subnet we are using to launch the instance
-    user_data = file("./nexus-server.sh")  # this is the script that will be executed during the creation of the instance
-    key_name = "terraform" # this is the key name that we have created in console
-    # iam_instance_profile = aws_iam_instance_profile.our-instance-profile.name
-    security_groups = [aws_security_group.our-security-group-for-nexus.id] # this is security grp in which we have openend ports
-    root_block_device {
-      volume_size = 20
-    }
-
-    tags = {
-        Name = "nexus-server"  # this will provide name to instance 
-    }
-}#!/bin/bash  
-
-###############################################################################################################
-#                   Docker Installation
-###############################################################################################################
-
-# Add Docker's official GPG key:
-# apt update -y
-# sudo apt-get install -y ca-certificates curl gnupg
-# sudo install -m 0755 -d /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-# sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# # Add the repository to Apt sources:
-# echo \
-#   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt-get update -y
-
-# sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-apt update -y
-apt install -y docker.io 
-
-docker run -d -p 8081:8081 --name nexus-server sonatype/nexus3
+}
